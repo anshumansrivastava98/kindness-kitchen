1ed320d650cd1e0d4b7dab468b4008c430edb28f
Anshuman Srivastava <anshumansrivastava11@gmail.com>
2024-05-07 01:05:25 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from "@angular/router";
 import {
-	HomepageComponent,
-	DonateFoodComponent,
-	ReceiveFoodComponent,
+    HomepageComponent,
+    DonateFoodComponent,
+    ReceiveFoodComponent,
 } from "../components/component-index";
 
 export const routes: Routes = [
-	{ path: "", component: HomepageComponent },
-	{ path: "donate-food", component: DonateFoodComponent },
-	{ path: "receive-food", component: ReceiveFoodComponent },
+    { path: "", component: HomepageComponent },
+    { path: "donate-food", component: DonateFoodComponent },
+    { path: "receive-food", component: ReceiveFoodComponent },
 ];
